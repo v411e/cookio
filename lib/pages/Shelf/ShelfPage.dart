ecd3abd82ce467a710cb4a45e72a6cca7052b878
Valentin Riess <valentin.riess@posteo.de>
2020-05-27 11:36:14 +0200

    Implemented drag and drop
    - Items in shelf can now be dragged between locations

--- lib/pages/Shelf/ShelfPage.dart
+++ lib/pages/Shelf/ShelfPage.dart
@@ -1,6 +1,9 @@
+import 'dart:collection';
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:search_widget/search_widget.dart';
-
 
 class ShelfPage extends StatefulWidget {
   ShelfPage();
@@ -12,6 +15,18 @@ class ShelfPage extends StatefulWidget {
 }
 
 class _ShelfPage extends State<ShelfPage> {
+  static const _target1 = 0;
+  static const _target2 = 1;
+
+  Map dragMap = {
+    new DraggableProduct(): _target1,
+    new DraggableProduct(): _target1,
+    new DraggableProduct(): _target1,
+    new DraggableProduct(): _target2,
+    new DraggableProduct(): _target2,
+    new DraggableProduct(): _target2,
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,84 +39,105 @@ class _ShelfPage extends State<ShelfPage> {
               selectedItemBuilder: null, //TODO: Implement
               queryBuilder: null, //TODO: Implement
             ),
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/shelf.png'),
-                    fit: BoxFit.fitWidth),
+            Text('Kühlschrank:'),
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: double.infinity,
+                color: Colors.grey,
+                child: DragTarget(
+                  builder: (BuildContext context,
+                      List<DraggableProduct> incoming, List rejected) {
+                    return Wrap(
+                      children: dragMaptoList(map: dragMap, target: _target1),
+                    );
+                  },
+                  onWillAccept: (data) => true,
+                  onAccept: (data) {
+                    print(data);
+                    setState(
+                      () {
+                        dragMap.update(data, (value) => _target1);
+                      },
+                    );
+                  },
+                  onLeave: (data) {},
+                ),
               ),
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    child: Image.asset(
-                      'assets/images/milk.png',
-                      height: 40.0,
-                    ),
-                    margin: EdgeInsets.only(
-                        left: 10.0, right: 10.0, bottom: 30.0),
-                  ),
-                  Container(
-                    child: Image.asset(
-                      'assets/images/milk.png',
-                      height: 40.0,
-                    ),
-                    margin: EdgeInsets.only(
-                        left: 10.0, right: 10.0, bottom: 30.0),
-                  ),
-                  Container(
-                    child: Image.asset(
-                      'assets/images/milk.png',
-                      height: 40.0,
-                    ),
-                    margin: EdgeInsets.only(
-                        left: 10.0, right: 10.0, bottom: 30.0),
-                  ),
-                  Container(
-                    child: Image.asset(
-                      'assets/images/milk.png',
-                      height: 40.0,
-                    ),
-                    margin: EdgeInsets.only(
-                        left: 10.0, right: 10.0, bottom: 30.0),
-                  ),
-                  Container(
-                    child: Image.asset(
-                      'assets/images/milk.png',
-                      height: 40.0,
-                    ),
-                    margin: EdgeInsets.only(
-                        left: 10.0, right: 10.0, bottom: 30.0),
-                  ),
-                ],
-              ),
-              margin: EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 10.0),
             ),
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage('assets/images/shelf.png'),
-                    fit: BoxFit.fitWidth),
+            Text('Regal:'),
+            Expanded(
+              flex: 1,
+              child: Container(
+                width: double.infinity,
+                color: Colors.grey,
+                child: DragTarget(
+                  builder: (BuildContext context,
+                      List<DraggableProduct> incoming, List rejected) {
+                    return Wrap(
+                      children: dragMaptoList(map: dragMap, target: _target2),
+                    );
+                  },
+                  onWillAccept: (data) => true,
+                  onAccept: (data) {
+                    //debugger();
+                    setState(
+                      () {
+                        dragMap.update(data, (value) => _target2);
+                      },
+                    );
+                  },
+                  onLeave: (data) {},
+                ),
               ),
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    child: Image.asset(
-                      'assets/images/milk.png',
-                      height: 40.0,
-                    ),
-                    margin: EdgeInsets.only(
-                        left: 10.0, right: 10.0, bottom: 30.0),
-                  ),
-                ],
-              ),
-              margin: EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 10.0),
-            ),
-            Container(
-              child: Image.asset('assets/images/shelf.png'),
-              margin: EdgeInsets.fromLTRB(10.0, 30.0, 10.0, 10.0),
             ),
           ],
         ),
+      ),
+    );
+  }
+
+  List<Widget> dragMaptoList({Map map, int target}) {
+    List<DraggableProduct> list = [];
+    map.forEach((key, value) {
+      if (value == target) {
+        list.add(key);
+      }
+    });
+    return list;
+  }
+}
+
+class DraggableProduct extends StatefulWidget {
+  DraggableProduct();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _DraggableProduct();
+  }
+}
+
+class _DraggableProduct extends State<DraggableProduct> {
+  @override
+  Widget build(BuildContext context) {
+    print('build ' + this.widget.hashCode.toString());
+    //debugger();
+    return Draggable(
+      data: this.widget,
+      child: Container(
+        color: Colors.lightGreen,
+        height: 50,
+        width: 50,
+      ),
+      feedback: Container(
+        color: Colors.orange,
+        height: 50,
+        width: 50,
+      ),
+      childWhenDragging: Container(
+        color: Colors.blue,
+        height: 50,
+        width: 50,
       ),
     );
   }
